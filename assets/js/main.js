d2f20658526833fe34ba012befebb807c0fe8d66
Darsh <darsh2987@gmail.com>
2017-04-17 21:53:58 +0100

    responsive design and all styling completed. added functionality for the booking system.

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,29 +7,38 @@ $(".hamburger-container").click(function() {
   $(".nav-menu").toggleClass("nav--reveal");
 });
 
+$(".bowling-icon__home-btn").mousedown(function() {
+  $(this).toggleClass("btn--scale");
+});
 
+$(".bowling-icon__home-btn").mouseup(function() {
+  $(this).toggleClass("btn--scale");
+});
 
+$(".nav li").mousedown(function() {
+  $(this).toggleClass("btn--scale");
+});
 
+$(".nav li").mouseup(function() {
+  $(this).toggleClass("btn--scale");
+});
 
+$(".book-now").click(function() {
+  $(".make-booking-modal").toggleClass("modal-fadein");
+});
 
+$(".modal__close").click(function() {
+  $(".make-booking-modal").toggleClass("modal-fadein");
+});
 
-
-
-/*
 document.querySelector(".btn").addEventListener("click", calc);
   function calc() {
-    var players = document.querySelector(".bowlers").value;
+    var adult = document.querySelector(".adult-bowlers").value;
+    var juniors = document.querySelector(".junior-bowlers").value;
     var games = document.querySelector(".games").value;
 
-    if (games == 1) {
-      document.querySelector(".totalPrice").textContent = "The total cost for your booking is " + "£" + players * games * 9;
+   var totalPrice = ((adult * 8 + juniors * 4) * games);
 
-    } else if (games == 2) {
-      document.querySelector(".totalPrice").textContent = "The total cost for your booking is " + "£" + players * games * 7.5;
-
-    } else if (games == 3) {
-      document.querySelector(".totalPrice").textContent = "The total cost for your booking is " + "£" + players * games * 6;
-    };
+    document.querySelector(".totalPrice").textContent = "The total cost for your booking is " + "£" + totalPrice;
 
 };
-*/
